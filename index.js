32261ebcc8211aee75ca88e0024d1a12af843e18
taylor.holmes <taylorh2343@gmail.com>
2022-01-31 03:33:12 -0600

    added prompt to manager

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+
 // node modules
 const fs = require("fs");
 const inquirer = require("inquirer");
@@ -9,20 +10,194 @@ const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern")
 
 
+//add dist outputs and path
+
+
+//team array
 const team = []
 
-function createTeam() {
-
-}
-
-function createIntern() {
-
-}
-
-function  CreateEngineer() {
-
-}
-
-function endTeamCreation() {
+// add manager prompts
+const createManager = () => {
+    console.log('Answer the prompts to enter manager information');
     
-}
+    inquirer.prompt ([
+        {
+            type: 'input',
+            name: 'name',
+            message: "I need to speak to your manager. What is your manager's name?",
+            validate: nameInput => {
+                if (nameInput) {
+                    return true;
+                } else {
+                    return 'Please enter managers name';
+                }
+            }
+        },
+
+        {
+            type: 'input',
+            name: 'id',
+            message: "What is your manager's ID badge number?",
+            validate: idInput => {
+                if (idInput) {
+                    return true;
+                } else {
+                    return 'Please enter managers badge number.';
+                }
+            } 
+        },
+        
+        {
+            type: 'input',
+            name: 'email',
+            message: "What is your manager's email?",
+            validate: emailInput => {
+                if (emailInput) {
+                    return true;
+                } else {
+                    return 'Please enter managers email.';
+                }
+            } 
+        },
+        
+        {
+            type: 'input',
+            name: 'office Number',
+            message: "What is your manager's office number?",
+            validate: officeInput => {
+                if (officeInput) {
+                    return true;
+                } else {
+                    return 'Please enter managers office number.';
+                }
+            } 
+        },   
+    ])
+    .then(managerInput => {
+        const { name, id, email, officeNumber } = managerInput;
+        const manager = new Manager(name, id, email, officeNumber);
+
+        teamArray.push(manager);
+        console.log(manager);
+
+    })
+};
+
+// add engineer prompts
+function createEngineer() {
+    console.log('Answer the prompts to enter engineer information');
+    
+    inquirer.prompt ([
+        {
+            type: 'input',
+            name: 'name',
+            message: " What is your engineer's name?",
+            validate: nameInput => {
+                if (nameInput) {
+                    return true;
+                } else {
+                    return "Please enter engineer's name";
+                }
+            }
+        },
+
+        {
+            type: 'input',
+            name: 'id',
+            message: "What is your engineers's ID badge number?",
+            validate: idInput => {
+                if (idInput) {
+                    return true;
+                } else {
+                    return "Please enter engineer's badge number.";
+                }
+            } 
+        },
+        
+        {
+            type: 'input',
+            name: 'email',
+            message: "What is your engineer's email?",
+            validate: emailInput => {
+                if (emailInput) {
+                    return true;
+                } else {
+                    return "Please enter engineer's email.";
+                }
+            } 
+        },
+        
+        {
+            type: 'input',
+            name: 'gitHub',
+            message: "What is your engineer's gitHub?",
+            validate: gitHubInput => {
+                if (gitHubInput) {
+                    return true;
+                } else {
+                    return "Please enter Engineer's gitHub.";
+                }
+            } 
+        },   
+    ]);
+};
+
+// add intern prompts
+function createIntern() {
+    console.log('Answer the prompts to enter intern information');
+    
+    inquirer.prompt ([
+        {
+            type: 'input',
+            name: 'name',
+            message: "What is your intern's name?",
+            validate: nameInput => {
+                if (nameInput) {
+                    return true;
+                } else {
+                    return "Please enter intern's name.";
+                }
+            }
+        },
+
+        {
+            type: 'input',
+            name: 'id',
+            message: "What is your intern's ID badge number?",
+            validate: idInput => {
+                if (idInput) {
+                    return true;
+                } else {
+                    "Please enter intern's badge number.";
+                }
+            } 
+        },
+        
+        {
+            type: 'input',
+            name: 'email',
+            message: "What is your intern's email?",
+            validate: emailInput => {
+                if (emailInput) {
+                    return true;
+                } else {
+                    return "Please enter intern's email.";
+                }
+            } 
+        },
+        
+        {
+            type: 'input',
+            name: 'schoolName',
+            message: "What is your intern's school name?",
+            validate: schoolInput => {
+                if (schoolInput) {
+                    return true;
+                } else {
+                    return "Please enter intern's school.";
+                }
+            } 
+        },   
+    ]);
+};
+
